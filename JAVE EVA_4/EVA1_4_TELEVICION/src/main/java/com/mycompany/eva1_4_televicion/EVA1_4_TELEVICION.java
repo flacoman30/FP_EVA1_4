cfa5f436487084a50176af2d58eb92445dac6be0
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-08-28 16:12:32 -0600

    Add files via upload

---
+++ JAVE EVA_4/EVA1_4_TELEVICION/src/main/java/com/mycompany/eva1_4_televicion/EVA1_4_TELEVICION.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_televicion;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA1_4_TELEVICION {
+
+    public static void main(String[] args) {
+       //ASIGNAR VALORES
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        //ASIGNAR VALORES
+        marca = "Samsung";
+        modelo = "Crystal";
+        precio = 8000;
+        tamano = "50";
+       System.out.println(marca);
+       System.out.println(modelo);
+       System.out.println(year);
+       System.out.println(tamano);
+       System.out.println(precio);     
+    }
+}
